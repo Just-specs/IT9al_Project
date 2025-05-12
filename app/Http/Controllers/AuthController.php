09f787a89607e49bef7f612adcb141e3ff55ec01
Mark Clarence Balmes <balmesmarkclarence@gmail.com>
2025-05-12 21:25:22 +0800

    admin seeder

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => 'admin'
+            'role' => 'employee'
         ]);
 
         return redirect()->route('login');
